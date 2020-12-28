1b9cbd7918ac2ba4b82618922e747ab0019c0b83
Sascha Bartl <bartl.sascha0@gmail.com>
2020-12-29 00:31:40 +0100

    Support netstandard2.0

--- Ghostscript.NET/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("5b1fe89e-e040-4b7a-9c05-478c1756b6e8")]
